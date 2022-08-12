a8e45417fe574fe4bd05e3b12a285887577404f1
“your <it20154226@my.sliit.lk>
2022-08-12 08:05:31 +0530

    Search done kinda

--- google-clone/src/components/Search.js
+++ google-clone/src/components/Search.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import MicIcon from "@mui/icons-material/Mic";
 import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
 
 function Search() {
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon />
+      </div>
+      <div className="search__buttons">
+        <Button onClick={search} variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
   );
